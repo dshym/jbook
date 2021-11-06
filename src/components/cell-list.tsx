c651e7df4e861d8ba185d44481ae2c2afb9a7edb
Dmytro Shymanskyi <dmytro.shymanskyi.dev@gmail.com>
2021-11-06 18:26:54 +0100

    added cumulative code execution and show function, some css improvements

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import './cell-list.css';
 import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
@@ -15,7 +16,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <div className={cells.length === 0 ? 'force-visible' : ''}>
         <AddCell previousCellId={null} />
       </div>
